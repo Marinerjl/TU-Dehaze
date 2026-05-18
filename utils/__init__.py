b7078498c62dba31bab6480e39a750a2a2799c1c
rjl <96274950+Marinerjl@users.noreply.github.com>
2026-05-18 17:47:48 +0800

    Add files via upload

---
+++ utils/__init__.py
@@ -0,0 +1,2 @@
+from .common import read_img, write_img, hwc_to_chw, chw_to_hwc, pad_img, init_weights,batch_PSNR, batch_SSIM, AverageMeter
+# from .scheduler import CosineScheduler
---
+++ utils/common.py
@@ -0,0 +1,166 @@
+import numpy as np
+import cv2
+import torch.nn.functional as F
+from skimage import img_as_ubyte
+import math
+
+def read_img(filename, to_float=True):
+	img = cv2.imread(filename)
+	if to_float: img = img.astype('float32') / 255.0
+	return img[:, :, ::-1]
+
+
+def write_img(filename, img, to_uint=True):
+	if to_uint: img = np.round(img * 255.0).astype('uint8')
+	cv2.imwrite(filename, img[:, :, ::-1])
+
+
+def hwc_to_chw(img):
+	return np.transpose(img, axes=[2, 0, 1]).copy()
+
+
+def chw_to_hwc(img):
+	return np.transpose(img, axes=[1, 2, 0]).copy()
+
+
+def pad_img(x, patch_size):
+	_, _, h, w = x.size()
+	mod_pad_h = (patch_size - h % patch_size) % patch_size
+	mod_pad_w = (patch_size - w % patch_size) % patch_size
+	x = F.pad(x, (0, mod_pad_w, 0, mod_pad_h), 'reflect')
+	return x
+
+def calculate_psnr(im1, im2, border=0):
+    if not im1.shape == im2.shape:
+        raise ValueError('Input images must have the same dimensions.')
+    h, w = im1.shape[:2]
+    im1 = im1[border:h-border, border:w-border]
+    im2 = im2[border:h-border, border:w-border]
+
+    im1 = im1.astype(np.float64)
+    im2 = im2.astype(np.float64)
+    mse = np.mean((im1 - im2)**2)
+    if mse == 0:
+        return float('inf')
+    return 20 * math.log10(255.0 / math.sqrt(mse))
+
+def batch_PSNR(img, imclean, border=0):
+    Img = img.data.cpu().numpy()
+    Iclean = imclean.data.cpu().numpy()
+    Img = img_as_ubyte(Img)
+    Iclean = img_as_ubyte(Iclean)
+    PSNR = 0
+    for i in range(Img.shape[0]):
+        PSNR += calculate_psnr(Iclean[i,:,].transpose((1,2,0)), Img[i,:,].transpose((1,2,0)), border)
+    return (PSNR/Img.shape[0])
+
+def ssim(img1, img2):
+    C1 = (0.01 * 255)**2
+    C2 = (0.03 * 255)**2
+
+    img1 = img1.astype(np.float64)
+    img2 = img2.astype(np.float64)
+    kernel = cv2.getGaussianKernel(11, 1.5)
+    window = np.outer(kernel, kernel.transpose())
+
+    mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]  # valid
+    mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
+    mu1_sq = mu1**2
+    mu2_sq = mu2**2
+    mu1_mu2 = mu1 * mu2
+    sigma1_sq = cv2.filter2D(img1**2, -1, window)[5:-5, 5:-5] - mu1_sq
+    sigma2_sq = cv2.filter2D(img2**2, -1, window)[5:-5, 5:-5] - mu2_sq
+    sigma12 = cv2.filter2D(img1 * img2, -1, window)[5:-5, 5:-5] - mu1_mu2
+
+    ssim_map = ((2 * mu1_mu2 + C1) * (2 * sigma12 + C2)) / ((mu1_sq + mu2_sq + C1) *
+                                                            (sigma1_sq + sigma2_sq + C2))
+    return ssim_map.mean()
+
+def calculate_ssim(img1, img2, border=0):
+    '''calculate SSIM
+    the same outputs as MATLAB's
+    img1, img2: [0, 255]
+    '''
+    if not img1.shape == img2.shape:
+        raise ValueError('Input images must have the same dimensions.')
+    h, w = img1.shape[:2]
+    img1 = img1[border:h-border, border:w-border]
+    img2 = img2[border:h-border, border:w-border]
+
+    if img1.ndim == 2:
+        return ssim(img1, img2)
+    elif img1.ndim == 3:
+        if img1.shape[2] == 3:
+            ssims = []
+            for i in range(3):
+                ssims.append(ssim(img1[:,:,i], img2[:,:,i]))
+            return np.array(ssims).mean()
+        elif img1.shape[2] == 1:
+            return ssim(np.squeeze(img1), np.squeeze(img2))
+    else:
+        raise ValueError('Wrong input image dimensions.')
+
+def batch_SSIM(img, imclean, border=0):
+    Img = img.data.cpu().numpy()
+    Iclean = imclean.data.cpu().numpy()
+    Img = img_as_ubyte(Img)
+    Iclean = img_as_ubyte(Iclean)
+    SSIM = 0
+    for i in range(Img.shape[0]):
+        SSIM += calculate_ssim(Iclean[i,:,].transpose((1,2,0)), Img[i,:,].transpose((1,2,0)), border)
+    return (SSIM/Img.shape[0])
+
+from torch.nn import init
+def init_weights(net, init_type='normal', init_gain=0.02):
+    """Initialize network weights.
+
+    Parameters:
+        net (network)   -- network to be initialized
+        init_type (str) -- the name of an initialization method: normal | xavier | kaiming | orthogonal
+        init_gain (float)    -- scaling factor for normal, xavier and orthogonal.
+
+    We use 'normal' in the original pix2pix and CycleGAN paper. But xavier and kaiming might
+    work better for some applications. Feel free to try yourself.
+    """
+    def init_func(m):  # define the initialization function
+        # 这行代码用于获取 PyTorch 模型中当前模块 m 的类名
+        classname = m.__class__.__name__
+        # 如果模块 m 具有 'weight' 属性，并且其类名中包含 'Conv' 或 'Linear'，则执行条件语句块中的初始化操作。
+        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+            # 用于对张量进行正态分布初始化。具体而言，它会将张量中的元素从正态分布（均值为 mean，标准差为 std）中随机采样，并用这些采样值填充张量。
+            if init_type == 'normal':
+                init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming':
+                init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                init.orthogonal_(m.weight.data, gain=init_gain)
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+            if hasattr(m, 'bias') and m.bias is not None:
+                init.constant_(m.bias.data, 0.0)
+        # BatchNorm 层的权重不是矩阵；只适用于正态分布。
+        elif classname.find('BatchNorm2d') != -1:  # BatchNorm Layer's weight is not a matrix; only normal distribution applies.
+            init.normal_(m.weight.data, 1.0, init_gain)
+            init.constant_(m.bias.data, 0.0)
+
+    print('initialize network with %s' % init_type)
+    # 将初始化函数应用到网络的所有模块上。
+    net.apply(init_func)  # apply the initialization function <init_func>
+
+class AverageMeter(object):
+	def __init__(self):
+		self.reset()
+
+	def reset(self):
+		self.val = 0
+		self.avg = 0
+		self.sum = 0
+		self.count = 0
+
+	def update(self, val, n=1):
+		self.val = val
+		self.sum += val * n
+		self.count += n
+		self.avg = self.sum / self.count
